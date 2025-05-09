7b64201b4715479f2b2a3dbf784eda15e72d99df
yoshi-networks <kaig5981@mytusd.org>
2025-05-09 09:26:13 -0700

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,8 @@ let messagesLoaded = false;
 // Update the bad words list and add enhanced filtering
 const BAD_WORDS = [
     'fuck', 'shit', 'ass', 'bitch', 'dick', 'pussy', 'cock', 'cunt', 'bastard',
-    'damn', 'hell', 'piss', 'whore', 'slut', 'retard', 'nigger', 'faggot'
+    'damn', 'hell', 'piss', 'whore', 'slut', 'retard', 'nigger', 'faggot', 'nigga', 'kai'
+
 ];
 
 // Add spam prevention variables
