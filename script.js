c41a1bb0be4eecae19cfcfa8f4f2b8052d33bca7
yoshi-networks <kaig5981@mytusd.org>
2025-10-21 14:15:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ function showDebugBanner(msg, timeout = 8000) {
 
 /* BAD WORDS LIST (unchanged) */
 const BAD_WORDS = [
-    'fuck', 'shit', 'ass', 'bitch', 'dick', 'pussy', 'cock', 'cunt', 'bastard',
+    'fuck', 'shit', 'ass', 'bitch', 'dick', 'pussy', 'cock', 'cunt', 'bastard', 'nigga', 'niggas', 'garvin'
     'damn', 'hell', 'piss', 'whore', 'slut', 'retard', 'nigger', 'faggot', 'kai', 'fucking', 'kais', 'niggers'
 ];
 
